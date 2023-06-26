018e8a7c8e1dfeeef455f02e0e9ee04b61235663
Hao Lam <tuonghao2001@gmail.com>
2023-06-26 03:57:06 -0700

    Coding Challenge #1

--- script.js
+++ script.js
@@ -43,8 +43,8 @@ const transformer = function (str, fn) {
   console.log(`Tranformed string: ${fn(str)} by ${fn.name} function`);
 };
 transformer('Javascript is the best!', oneWord);
-const high5 = () => console.log('👋');
-document.body.addEventListener('click', high5);
+// const high5 = () => console.log('👋');
+// document.body.addEventListener('click', high5);
 // 2. function that returns new function
 const greet = greeting => name => console.log(`${greeting} ${name}`);
 const greetHey = greet('Hey');
@@ -72,6 +72,67 @@ const getAgeCat1 = getAge.bind(cat);
 console.log('Bind method');
 getAgeCat1('Cat'); // 21
 getAge.bind(cat)('Cat');
-const getAgeCat2 = getAge.bind(cat, 'Cat')
+const getAgeCat2 = getAge.bind(cat, 'Cat');
 getAgeCat2(); // 21
 getAge.bind(cat, 'Cat')(); // 21
+// bind with eventListner
+hao.planes = 10;
+hao.buyPlane = function () {
+  this.planes++;
+  console.log(this.planes);
+};
+// document.querySelector('.buy').addEventListener('click', hao.buyPlane); // NaN
+document
+  .querySelector('.buy')
+  .addEventListener('click', hao.buyPlane.bind(hao)); // 11
+
+// Partial Application (not interested in this keyword, but interested in preset parameters)
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(100));
+console.log(addVAT(23));
+const addTax2 = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const addVAT2 = addTax2(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
+
+// Coding Challenge #1
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    const idx = Number(
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`
+      )
+    );
+    typeof idx === 'number' &&
+      idx >= 0 &&
+      idx < this.answers.length &&
+      this.answers[idx]++;
+    this.displayResults('string');
+  },
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
+  },
+};
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+const data1 = [5, 2, 3];
+const data2 = [1, 5, 3, 9, 6, 1];
+const displayPoll = poll.displayResults;
+displayPoll.call({ answers: data1 });
+displayPoll.call({ answers: data1 }, 'string');
+displayPoll.call({ answers: data2 });
+displayPoll.call({ answers: data2 }, 'string');
